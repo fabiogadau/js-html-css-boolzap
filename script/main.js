997eaaf60e1be8de1ca26de4742f57d377e9c102
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-17 11:24:21 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,51 @@
+/*
+* Boolzap
+Replica della grafica (allegata sotto con gli assets) con la possibilità di avere messaggi stilati e posizionati diversamente in base a: messaggio dall’utente (verdi) e messaggio dall’interlocutore (bianco) assegnando due classi CSS diverse.
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando icona ‘invia il testo’ viene aggiunto al thread sopra, come messaggio verde (ricordate focus() )
+Font family: Lato
+Messaggi visibili inizialmente sono inseriti statici nell’HTML
+Usate un template nell’html e clone() per l’ inserimento del messaggio da fare in JS
+*/
+$( document ).ready(function() {
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
